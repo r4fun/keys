6823fd8e30308ebf18fff2bd9c5dfec7daf78035
tylurp <tylurp1@gmail.com>
2020-10-07 12:11:37 -0700

    add example

--- R/record.R
+++ R/record.R
@@ -6,6 +6,33 @@
 #' @param inputId The input slot that will be used to access the value.
 #' @export
 #' @rdname recordKeys
+#' @examples
+#' if (interactive()) {
+#'   library(shiny)
+#'
+#'   ui <- fluidPage(
+#'     useKeys(),
+#'     keysRecordInput("recorder"),
+#'     keysInput("keys", "command+shift+k"),
+#'     actionButton("record", "Record keys")
+#'   )
+#'
+#'   server <- function(input, output, session) {
+#'     observeEvent(input$record, {
+#'       print("recording keys...")
+#'       recordKeys("recorder")
+#'     })
+#'     observeEvent(input$recorder, {
+#'       print("adding keys...")
+#'       addKeys("keys", input$recorder)
+#'     })
+#'     observeEvent(input$keys, {
+#'       print(input$keys)
+#'     })
+#'   }
+#'
+#'   shinyApp(ui, server)
+#' }
 keysRecordInput <- function(inputId) {
   htmltools::tagList(
     htmltools::tags$input(
